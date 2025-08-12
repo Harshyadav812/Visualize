3860290cb655744da5e0b099b9987131d6e532fd
Harshyadav812 <harshrao812@gmail.com>
2025-08-12 02:59:54 +0000

    Refactor CSS variables to use design tokens and enhance theme management

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import './styles/transitions.css'
+import './styles/loading-indicators.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
